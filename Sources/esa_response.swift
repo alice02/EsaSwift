d4544474c889fa8320855636d61079eaeb3fe751
Kouta ASAI <scene.4913@gmail.com>
2017-02-05 22:56:49 +0900

    [update] change response body type Map to Any

--- Sources/esa_response.swift
+++ Sources/esa_response.swift
@@ -1,40 +1,47 @@
 import HTTP
 import Axis
+import Foundation
 
 struct EsaResponse {
 
     private var raw_body: Body?
     private var raw_headers: Headers?
-    private var raw_status: Response.Status?
+    private var raw_status: Int?
     
     init(response: Response) {
         self.raw_body = response.body
         self.raw_headers = response.headers
-        self.raw_status = response.status
+        self.raw_status = response.statusCode
     }
 
-    var body: Map {
+    var body: Any {
         get {
             do {
                 var body = self.raw_body!
                 let buffer = try body.becomeBuffer(deadline: 3.second.fromNow())
-                let map = try JSONMapParser.parse(buffer)
-                return map
+                let data = Data(bytes: buffer.bytes)
+                let json = try JSONSerialization.jsonObject(with: data)
+                return json
             } catch {
-                return nil
+                // TODO Error Handling
+                return [:]
             }
         }
     }
     
-    var headers: Headers {
+    var headers: [String : String] {
         get {
-            return self.raw_headers!
+            var headers_dict: [String: String] = [:]
+            for (header, value) in self.raw_headers! {
+                headers_dict[header.description] = value
+            }
+            return headers_dict
         }
     }
     
     var status: Int {
         get {
-            return self.raw_status!.hashValue
+            return self.raw_status!
         }
     }
 }
