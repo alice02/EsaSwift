5ea4401d96e0e40ceaa6e1150872b47841ac50de
Kouta ASAI <scene.4913@gmail.com>
2017-02-15 01:11:44 +0900

    [add] HTTP client class

---
+++ Sources/HTTPClient.swift
@@ -0,0 +1,109 @@
+import Foundation
+
+class HTTPClient {
+    let session: URLSession = URLSession(configuration: .default)
+    var base_url: String
+
+    enum HTTPMethod {
+        case get
+        case post
+        case put
+        case patch
+        case delete
+    }
+
+    init(url: String) {
+        self.base_url = url
+    }
+
+    func request(method: HTTPMethod, path: String, params: Dictionary<String, String>? = nil, headers: Dictionary<String, String>? = nil, body: Data? = nil) -> Response {
+        var request: URLRequest
+        var urlComponents = URLComponents(string: base_url)!
+        urlComponents.path = path
+
+        if let queryParams = params {
+            var queryItems: [URLQueryItem] = []
+            for (key, value) in queryParams {
+                queryItems.append(URLQueryItem(name: key, value: value))
+            }
+            urlComponents.queryItems = queryItems
+        }
+
+        request = URLRequest(url: urlComponents.url!)
+        if let headerItems = headers {
+            for (key, value) in headerItems {
+                request.addValue(value, forHTTPHeaderField: key)
+            }
+        }
+
+        if let httpBody = body {
+            request.httpBody = httpBody
+        }
+        
+        switch method {
+        case .get:
+            request.httpMethod = "GET"
+        case .post:
+            request.httpMethod = "POST"
+        case .put:
+            request.httpMethod = "PUT"
+        case .patch:
+            request.httpMethod = "PATCH"
+        case .delete:
+            request.httpMethod = "DELETE"
+        }
+
+        return task(request: request)
+    }
+
+    func task(request: URLRequest) -> Response {
+        var response: URLResponse?
+        var data: Data?
+        var error: Error?
+        let condition = NSCondition()
+
+        session.dataTask(with: request,
+                         completionHandler: { (dat, res, err) in
+                             data = dat
+                             error = err
+                             response = res
+                             condition.broadcast()
+                         }).resume()
+        condition.wait()
+        if error != nil {
+            // TODO Error Handling
+            print("HTTPRequestError")
+        }
+
+        return Response(data: data, response: response)
+    }
+}
+    
+struct Response {
+
+    private var raw_data: Data
+    private var raw_response: HTTPURLResponse
+    
+    init(data: Data?, response: URLResponse?) {
+        self.raw_data = data!
+        self.raw_response = response! as! HTTPURLResponse
+    }
+
+    var body: Data? {
+        get {
+            return raw_data
+        }
+    }
+    
+    var headers: Any {
+        get {
+            return raw_response.allHeaderFields
+        }
+    }
+    
+    var status: Int {
+        get {
+            return self.raw_response.statusCode
+        }
+    }
+}
