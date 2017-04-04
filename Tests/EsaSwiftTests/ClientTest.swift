ecfb2e9d21909954acb41889e9350097b0c48011
Kouta Asai <scene.4913@gmail.com>
2017-04-05 02:22:42 +0900

    [add] header test of esa client

--- Tests/EsaSwiftTests/ClientTest.swift
+++ Tests/EsaSwiftTests/ClientTest.swift
@@ -7,13 +7,25 @@ class EsaClientTests: XCTestCase {
         let token = "token"
         let team = "team"
         let client = EsaClient(access_token: token, current_team: team)
-        
+
         XCTAssertEqual(client.current_team, team)
+        XCTAssertEqual(client.access_token, token)
+    }
+
+    func testHeaders() {
+        let token = "token"
+        let team = "team"
+        let client = EsaClient(access_token: token, current_team: team)
+
+        XCTAssertEqual(client.header["Accept"], "application/json")
+        XCTAssertEqual(client.header["Authorization"], "Bearer \(token)")
+        XCTAssertEqual(client.header["Content-Type"], "application/json")
     }
 
     static var allTests : [(String, (EsaClientTests) -> () throws -> Void)] {
         return [
           ("testCurrentTeam", testCurrentTeam),
+          ("testHeaders", testHeaders),
         ]
-    }   
+    }
 }
